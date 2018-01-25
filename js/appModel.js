763d7c33beeab2052475c9257bd8bac4748a5b2a
Tadpol Rachatasumrit <tadpolr@trmbp.local>
2018-01-25 16:49:44 -0500

    Fixed from code review

--- js/appModel.js
+++ js/appModel.js
@@ -3,84 +3,24 @@ const id = "LSBXWNV1MELUFVDXJAL5H0YMLM1ODQBBJIHOFN2MNAGMINI0";
 const secret = "GHFZ2SFD2UVD1JDPCMEEF3PRNZSHK143NYQBPY0PGXV0YZBF";
 const v = 20180124;
 const ll = "40.443520,-79.942936";
-const limitRes = 10;
-const radius = 2500;
+const limitRes = 50;
+const radius = 2000;
 
-// This function is a async function that will receieve shop catagory as string and return an array of recommended shops.
-function getRecommendShop(data) {
-    const catagory = data ? data : "food";
+// This function is a async function that will receieve shop catagory as string and 
+// return an array of recommended shops.
+function getRecommendedShop(data) {
     return fetch(
-        `https://api.foursquare.com/v2/venues/explore?client_id=${id}&client_secret=${secret}&ll=${ll}&v=${v}&limit=${limitRes}&radius=${radius}&query=${catagory}`,
+        `https://api.foursquare.com/v2/venues/explore?client_id=${id}&client_secret=${secret}&ll=${ll}&v=${v}&limit=${limitRes}&radius=${radius}&query=food`,
         {method: 'GET'}
     )
     .then(function(res) {
         return res.json();
     })
-    // .then(function(res) {
-    //     let promises = [];
-    //     let resArr = [];
-    //     res.response.groups[0].items.forEach(e => {
-    //         promises.push(
-    //             getVenueDetail(e.venue.id)
-    //             .then(function(res) {
-    //                 resArr.push(res.response);
-    //             })
-    //         );
-    //     });
-    //     return Promise.all(promises).then(function() {
-    //         return resArr;
-    //     });
-    // })
+    .then(function(res) {
+        return res.response.groups[0].items;
+    })
     .catch(function(err) {
         const messege = `Cannot get recommended shop\n${err}`;
         alert(messege);
     });
 }
-
-// This function is a async function that will receieve search input as string and return an array of matching shops.
-function searchShop(input) {
-    return fetch(
-        `https://api.foursquare.com/v2/venues/search?client_id=${id}&client_secret=${secret}&ll=${ll}&v=${v}&limit=${limitRes}&radius=${radius}&query=${input}`,
-        {method: 'GET'}
-    )
-    .then(function(res) {
-        return res.json();
-    })
-    // .then(function(res) {
-    //     let promises = [];
-    //     let resArr = [];
-    //     res.response.venues.forEach(e => {
-    //         promises.push(
-    //             getVenueDetail(e.id)
-    //             .then(function(res) {
-    //                 resArr.push(res.response);
-    //             })
-    //         );
-    //     });
-    //     return Promise.all(promises).then(function(){
-    //         return resArr;
-    //     });
-    // })
-    .catch(function(err) {
-        const messege = `Cannot search shop\n${err}`;
-        alert(messege);
-    });
-}
-
-// This function is a async function that will receieve shop's id and return an object that contains information of that shop.
-// This function is created to ensure that every shop's object has the same format.
-
-// function getVenueDetail(venueId) {
-//     return fetch(
-//         `https://api.foursquare.com/v2/venues/${venueId}?client_id=${id}&client_secret=${secret}&v=${v}`,
-//         {method: 'GET'}
-//     )
-//     .then(function(res) {
-//         return res.json();
-//     })
-//     .catch(function(err) {
-//         const messege = `Cannot get shop's detail\n${err}`;
-//         alert(messege);
-//     });
-// }
-
--- js/appVM.js
+++ js/appVM.js
@@ -1,133 +1,179 @@
 function AppViewModel() {
     const self = this;
 
-    // These arrays are used to store markers and infoWindows that are created by createMarker function.
-    let markers = ko.observableArray();
-    let infoWindows = ko.observableArray();
+    // This is an array of all shop objects from foursquare api request.
+    self.shops = ko.observableArray([]);
 
-    // This function is used to create markers and infoWindows by passing an array of shops.
-    // It also bind each marker with related infoWindows by click event.
-    self.createMarker = function(shops) {
-        // Remove all existing markers
-        markers().forEach(e => {
-            e.setMap(null);
-        });
-        markers.removeAll();
-
-        // This function is used to stop all markers animation and start bouncing selected marker. 
-        function toggleBounce(marker) {
-            markers().forEach(e => {
-                e.setAnimation(null);
-            });
-            marker.setAnimation(google.maps.Animation.BOUNCE);
-        }
-
-        // Create markers by looping on shops array
-        for (let i = 0; i < shops.length; i++) {
-            const shop = shops[i];
-            const shopInfo = shop.venue;
-            const lat = shopInfo.location.lat;
-            const lng = shopInfo.location.lng;
-            
-            const marker = new google.maps.Marker({
-                position: {lat, lng},
-                map: map,
-                animation: google.maps.Animation.DROP,
-                title: shopInfo.name
-            });
-            markers.push(marker);
-            
-            // Create an infoWindow containing shop's information.
-            const infoWindow = new google.maps.InfoWindow({
-                content: `<h3>${shopInfo.name}</h3>
-                <p>${shopInfo.description}</p>
-                <p>
-                    <strong>tel:</strong> ${shopInfo.contact.formattedPhone} <br />
-                    <strong>address:</strong> ${shopInfo.location.formattedAddress.join(",")}
-                </p>
-                `
-            });
-            infoWindows.push(infoWindow);
-            
-            // Set event's listener for clicking marker.
-            marker.addListener('click', function() {
-                
-                // Make clicked marker bounce
-                toggleBounce(marker);
-
-                // Hidden all showing infoWindows and open current shop infoWindow.
-                infoWindows().forEach(e => {
-                    e.close();
-                });
-                infoWindow.open(map, marker);
-            });
-        }
+    // This is an array of current displayed shops.
+    // An update function receive shop object as an agrument, 
+    // then show its marker and push it into the array. 
+    self.showingShops = ko.observableArray([]);
+    self.updateShowingShops = function(shop) {
+        self.showMarker(shop);
+        self.showingShops.push(shop);
     }
 
-    // This array store shopLists that are created by createShopList function.
-    self.shopList = ko.observableArray()
-    self.createShopList = function(shops) {
-        self.shopList.removeAll();
-        shops.forEach(e => {
-            self.shopList.push(e.venue);
+    // These functions, which are used to create and manipulate markers.
+
+    // This function receive shop object as an agrument,
+    // then create marker with its information and set marker as object's property.
+    self.createMarker = function(shop) {
+        const info = shop.venue;
+        const lat = info.location.lat;
+        const lng = info.location.lng;
+        
+        const marker = new google.maps.Marker({
+            position: {lat, lng},
+            map: map,
+            animation: google.maps.Animation.DROP,
+            title: info.name
+        });
+        shop.marker = marker
+    }
+    // This function receive shop object as an agrument, then set its marker status into visible.
+    self.showMarker = function(shop) {
+        shop.marker.setMap(map);
+    }
+    // This function is used to make all markers invisible.
+    self.hideAllMarker = function() {
+        self.shops().forEach(shop => {
+            shop.marker.setMap(null);
         });
     }
 
-    // This function is used to automatically click marker when user click the shopList with the same shop's information.
-    self.clickListHandler= function(data) {
-        markers().forEach(function(e) {
-            if (data.name === e.title) {
-                google.maps.event.trigger(e, 'click');
+    // These functions, which are used to create and manipulate infoWindows.
+
+    // This function receive shop object as an agrument,
+    // then create infoWindow with its information and set infoWindow as object's property.
+    self.createInfoWindow = function(e) {
+        const info = e.venue;
+        const name = info.name ? info.name : "No name available.";
+        const tips = e.tips ? e.tips[0].text : "No tips available.";
+        const phone = info.contact.formattedPhone ? info.contact.formattedPhone : "No phone number available.";
+        const address = info.location.formattedAddress ? info.location.formattedAddress.join(",") : "No address available.";
+
+        const infoWindow = new google.maps.InfoWindow({
+            content: `<h3>${name}</h3>
+            <p>${tips}</p>
+            <p>
+                <strong>tel:</strong> ${phone} <br />
+                <strong>address:</strong> ${address} <br />
+            </p>
+            `
+        });
+        e.infoWindow = infoWindow
+    }
+    // This function receive shop object as an agrument. 
+    // Close all infoWindows and then open current shop infoWindow.
+    self.openInfoWindow = function(shop) {
+        self.closeAllInfoWindow();
+        shop.infoWindow.open(map, shop.marker);
+    }
+    // This function is used to close all infoWindows
+    self.closeAllInfoWindow = function() {
+        self.shops().forEach(e => {
+            e.infoWindow.close();
+        });
+    }
+
+    // This function receive string as an agrument and search for matching objects in self.shops array.
+    // Then push matching objects to self.showingShops by self.updateShowingShops function.
+    self.filterShops = function(input) {
+        self.showingShops.removeAll();
+        self.closeAllInfoWindow();
+        self.hideAllMarker();
+
+        self.shops().forEach(function(e) {
+            const name = e.venue.name;
+            const address = e.venue.location.address;
+            const category = e.venue.categories[0].name;
+            const shopKeyword = `${name} ${address} ${category}`.toLowerCase();
+
+            if (shopKeyword.includes(input)) {
+                self.updateShowingShops(e);
             }
         });
-    }
-
-    // This variable bind with input DOM element and its value will be used to search for shops. 
-    self.searchInput = ko.observable();
-    // This function will search for shops that match the input value by sending a foursquare's api request to foursquare api.
-    self.searchShop = function(){
-        const input = self.searchInput();
-
-        searchShop(input)
-        .then(function(res) {
-            self.createMarker(res);
-            self.createShopList(res);
-        })
-    }
-
-    // This function is binded with filter button. It will get new array of shops by sending a foursquare's api request, 
-    // and then create markers, infoWindows and shopList with that array.
-    self.filterHandler = function(data, event) {
-        getRecommendShop(event.target.id)
-        .then(function(res) {
-            self.createMarker(res);
-            self.createShopList(res);
-        })
     };
 
-    // This function will pick one shop randomly from markers.
-    self.randomShop = function() {
-        const ranNum = Math.round(( Math.random() * markers().length ) - 1);
-        google.maps.event.trigger(markers()[ranNum], 'click');
+    // This function is used to handle search feature.
+    self.searchInput = ko.observable();
+    self.searchHandler = function() {
+        self.filterShops(self.searchInput());
     }
 
-    // This function is called once when starting the app in order to create a map using google map apis,
-    // and then create markers, infoWindows and shopList from list of shops gotten by sending by 
-    // sending an api request to foursquare explore venue api.
+    // This function is used to handle filter feature.
+    self.filterCategories = ko.observableArray(['not-selected', 'american', 'sushi', 'chinese', 'mexican', 'cafe']);
+    self.chosenCategories = ko.observable();
+    self.filterHandler = function() {
+        if (self.chosenCategories() === 'not-selected') {
+            self.showingShops.removeAll();
+            self.closeAllInfoWindow();
+            self.hideAllMarker();
+
+            self.shops().forEach(e => {
+                self.updateShowingShops(e);
+            });
+        } else {
+            self.filterShops(self.chosenCategories());
+        }
+    }
+
+    // This function is used to bounce marker and open infoWindow when click at sidebar list.
+    self.clickListHandler = function(shop) {
+        google.maps.event.trigger(shop.marker, 'click');
+    }
+
+    // This function is used to handle random feature which will random a shop from self.shops array
+    self.randomShop = function() {
+        const ranNum = Math.ceil(( Math.random() * self.showingShops().length ) - 1);
+        google.maps.event.trigger(self.showingShops()[ranNum].marker, 'click');
+    }
+    
+    // This function is called once when the googlemap api script is loaded.
+    // It setups the main map, get recommended shops array by calling getRecommendedShop function,
+    // and then loop to shops array in order to create markers, infoWindows, and set their event's listener.
     let map;      
-    function init() {
+    self.init = function() {
         map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 40.4435904, lng: -79.9447279},
         zoom: 14
         });
-        getRecommendShop()
+
+        getRecommendedShop()
         .then(function(res) {
-            self.createMarker(res);
-            self.createShopList(res);
+            res.forEach(e => {
+                self.shops().push(e);
+            });
         })
+        .then(function() {
+            self.shops().forEach(e => {
+                self.createMarker(e);
+                self.createInfoWindow(e);
+                
+                e.marker.addListener('click', function() {
+                    // Stop bouncing marker and bouce current marker.
+                    self.shops().forEach(e => {
+                        e.marker.setAnimation(null);
+                    });
+                    this.setAnimation(google.maps.Animation.BOUNCE);
+    
+                    self.openInfoWindow(e);
+                });
+    
+                self.updateShowingShops(e);
+            });
+        })
+        .catch(function() {
+            alert('cannot create markers or infoWindows');
+        });
     }
-    AppViewModel.init = init;
-    // init();
+    AppViewModel.init = self.init;
 }
 
 ko.applyBindings(new AppViewModel());
+
+// This function is used to handle when google map api cannot load.
+function googleLoadedError() {
+    const messege = `google map is unable to load.\n${err}`;
+    alert(messege);
+}
